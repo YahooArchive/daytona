8d351882407c8640e4379966bf6d68c9bedecec7
Deepesh Mittal <dmittal@yahoo-inc.com>
2017-03-15 11:44:43 -0700

    adding authentication webservice

---
+++ UI/verifyuser.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: dmittal
+ * Date: 3/2/17
+ * Time: 3:40 PM
+ */
+
+$user = '';
+$password = '';
+
+if(isset($_POST["user"])) {
+    $user = $_POST["user"];
+}else{
+    echo http_response_code(400);
+    return;
+}
+
+if(isset($_POST["password"])) {
+    $password = $_POST["password"];
+}else{
+    echo http_response_code(400);
+    return;
+}
+
+require('lib/common.php');
+$db = initDB();
+
+if(validatePassword($db, $user,$password)) {
+    error_log(print_r("inside validate password",true));
+    echo http_response_code(200);
+    return;
+}
+else {
+    echo http_response_code(401);
+    return;
+}
